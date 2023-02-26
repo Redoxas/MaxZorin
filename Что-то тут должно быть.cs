d8d401f82a111f30a65b8bb9ccc4d93e6d4326bd
Макс <@->
2023-02-26 21:44:00 +0400

    Изменения

--- Что-то тут должно быть.cs
+++ Что-то тут должно быть.cs
@@ -1,15 +1,16 @@
-п»їusing System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace ConsoleApp2
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Привет мир!");
         }
     }
 }
